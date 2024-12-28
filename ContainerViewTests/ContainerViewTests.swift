273f9429ce9720666c63461483237ad205400b72
MacOS <ahmedawadallah@MacBook-Air.local>
2024-12-28 12:41:27 +0200

    upload project files

---
+++ ContainerViewTests/ContainerViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContainerViewTests.swift
+//  ContainerViewTests
+//
+//  Created by MacOS on 28/12/2024.
+//
+
+import Testing
+@testable import ContainerView
+
+struct ContainerViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
